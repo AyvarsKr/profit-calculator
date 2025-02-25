220947b2d2b479d5019c87e324c1c23042adf54d
Ayvars Krizhus~ <ayvars@bk.ru>
2025-02-25 14:29:40 +0300

    Обновления

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import ProfitCalculator from './ProfitCalculator'; // Убедитесь, что
 function App() {
   return (
     <div className="App">
-      <h1>Добро пожаловать в Калькулятор Прибыли!</h1>
+      <h1>Добро пожаловать в Калькулятор Прибыли от БОЁК ПРО!</h1>
+      <h1>ПОМНИ! БОЁК - ЭТО УДАР ПО ЦЕНАМ.</h1>
       <ProfitCalculator /> {/* Использование компонента */}
     </div>
   );
